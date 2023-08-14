4957f30811e880f10d0d65567a147fe6075669b0
Jitesh Kumar <jiteshece@gmail.com>
2023-08-15 01:17:02 +0530

    fixed error

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ Flask Application: This module defines a simple Flask web application.
 
 from flask_pymongo import PyMongo
 from flask import Flask
-from application import routes
+# from application import routes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '395633a8b3997cf522a2db01a9428092b649273d'
